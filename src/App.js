7b24af0d3036abd9f71c587dd7a74aa55ab4bcf8
ALEX LEONKOV <al@process-science.com>
2024-01-05 21:16:58 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "antd";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const fetchTheData = () => {
+    fetch("http://127.0.0.1:8080")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.text();
+      })
+      .then((data) => {
+        console.log("Data received from server:", data);
+        // Handle your data here
+
+        setData(data);
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Heimdall App</h2>
+
+      <Button onClick={fetchTheData} className="btn">
+        Load new data
+      </Button>
+
+      {data ? <div className="result">{data}</div> : <></>}
     </div>
   );
 }
